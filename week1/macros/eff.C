0210cc2b9490e6625871bd6027f112e40595db35
kelleyrw <kelleyrw@gmail.com>
2013-12-01 23:28:42 -0800

    removed commented code

--- week1/macros/eff.C
+++ week1/macros/eff.C
@@ -1,23 +1,4 @@
 {
-/*     tree->Draw("tps_p4.pt()>>hden(125,0,250)", "fabs(tps_p4.pt()>0.05) && tps_charge!=0 && tps_nhits>=3 && fabs(tps_dz)<30.0 && fabs(tps_d0)<3.5"); */
-/*     tree->Draw("tps_p4.pt()>>hnum(125,0,250)", "fabs(tps_p4.pt()>0.05) && tps_charge!=0 && tps_nhits>=3 && fabs(tps_dz)<30.0 && fabs(tps_d0)<3.5 && tps_matched"); */
-/*     heff_vs_eta = (TH1*)hnum->Clone("heff_vs_eta"); */
-/*     heff_vs_eta->Divide(hden); */
-/*     heff_vs_eta->Draw(); */
-
-/*         TpSelectorForEfficiencyVsEta = cms.PSet( */
-/*             chargedOnly = cms.bool(True), */
-/*             pdgId = cms.vint32(), */
-/*             tip = cms.double(2.0), */
-/*             signalOnly = cms.bool(True), */
-/*             minRapidity = cms.double(-2.5), */
-/*             lip = cms.double(30.0), */
-/*             ptMin = cms.double(0.9), */
-/*             maxRapidity = cms.double(2.5), */
-/*             minHit = cms.int32(0), */
-/*             stableOnly = cms.bool(False) */
-/*         ), */
-
     TH1::SetDefaultSumw2(true);
     tree->Draw("tps_p4.eta()>>h_den(50, -2.5, 2.5)", "tps_charge!=0 && tps_p4.pt()>0.9 && fabs(tps_p4.eta())<2.5 && tps_nhits>=3 && fabs(tps_lip)<30.0 && fabs(tps_tip)<3.5");
     tree->Draw("tps_p4.eta()>>h_num(50, -2.5, 2.5)", "tps_charge!=0 && tps_p4.pt()>0.9 && fabs(tps_p4.eta())<2.5 && tps_nhits>=3 && fabs(tps_lip)<30.0 && fabs(tps_tip)<3.5 && tps_matched");
